290ccfdbcc651415b9709b2923935d0b151bd88e
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2024-04-21 19:55:39 -0400

    Conexión con bd para registar e iniciar sesión con usuarios.

--- app/app.py
+++ app/app.py
@@ -1,34 +1,77 @@
-from flask import Flask
-from flask import render_template
-import requests
-
-url = "https://api.themoviedb.org/3/"
-
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMGI3OTVhMjQ2MzBkYTRhZjVkMTgwOTJkZGNlODE4MSIsInN1YiI6IjY2MjJhMTAwZTRjOWViMDBjN2Y0ZTFjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y6uNWZ--j5-V1-HTOdWk59EiCjvrNRMNHPzAwtu8ncg"
-}
-
+from flask import Flask, redirect, url_for, render_template, request, jsonify, session
+import psycopg2
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-# https://image.tmdb.org/t/p/original/[poster_path]
-
-# npx tailwindcss -i ./app/static/src/input.css -o ./app/static/src/main.css --watch
+# Conexión con la bd
+conn = psycopg2.connect(
+    dbname="bd_recomend",
+    user="postgres",
+    password="123456",
+    host="localhost"
+)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return redirect(url_for('start'))
 
-@app.route('/login')
+@app.route('/start')
+def start():
+    return render_template('start.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM public.\"Usuario\" WHERE usuario_id = %s", (email,))
+        result = cur.fetchone()
+
+        if result:
+            if result[1] == password:
+                session['email'] = email
+                name = result[2] 
+                return redirect(url_for('home', name=name))
+            else:
+                return jsonify({'error': 'Clave inválida.'}), 400
+        else:
+            return jsonify({'error': 'El correo ingresado no pertenece a ninguna cuenta.'}), 400
+
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+
+        fullname = request.form['fullname']
+        email = request.form['email']
+        password = request.form['password']
+        passwordcf = request.form['passwordcf']
+
+        if password != passwordcf:
+            return jsonify({'error': 'Las contraseñas no coinciden.'}), 400
+
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM public.\"Usuario\" WHERE usuario_id = %s", (email,))
+        result = cur.fetchone()[0]
+
+        if result > 0:
+            return jsonify({'error': 'El correo electrónico ya está registrado.'}), 400
+        else:
+            cur.execute("INSERT INTO public.\"Usuario\" (usuario_id, usuario_password, usuario_name) VALUES (%s, %s, %s)", (email, password, fullname))
+            conn.commit()
+            cur.close()
+            return jsonify({'success': 'Usuario registrado correctamente.'}), 200
+
     return render_template('register.html')
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/home/<name>')
+def home(name=None):
+    return render_template('home.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
