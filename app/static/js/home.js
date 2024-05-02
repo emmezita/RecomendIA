c05b71828bb59723e6782bdacb864f1e0c57fb15
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2024-05-01 23:14:28 -0400

    Interfaz de Vamos a conocerte lista

---
+++ app/static/js/home.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#generos').select2({
+        placeholder: 'Elige tu género favorito',
+    });
+
+    $('#directores').select2({
+        placeholder: 'Elige tu director favorito',
+    });
+
+    $('#epocas').select2({
+        placeholder: 'Elige tu época favorita',
+    });
+});
