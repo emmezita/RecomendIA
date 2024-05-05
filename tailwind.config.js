97e5847dfe4cb294927adf72f1467f06249eb8ec
Estefany Torres <emmezita@gmail.com>
2024-05-05 15:24:30 -0400

    Agregando el boton info a las peliculas

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,22 @@ module.exports = {
   content: ["./app/templates/**/*.{html,css,js}"],
   theme: {
     fontFamily: {
-      poppins : ['Poppins', 'sans-serif'],
-      hind : ['Hind', 'sans-serif'],
+      poppins: ["Poppins", "sans-serif"],
+      hind: ["Hind", "sans-serif"],
     },
     extend: {
       colors: {
-        'rojo-oscuro': '#8C2E2E',
-        'rojo-medio': '#B51F1F',
-        'rojo-claro': '#E00000',
-        'light': '#E0C9C9',
-        'dark': '#1B1E2F',
-        'dark-2': '#232538',
+        "rojo-oscuro": "#8C2E2E",
+        "rojo-medio": "#B51F1F",
+        "rojo-claro": "#E00000",
+        light: "#E0C9C9",
+        dark: "#1B1E2F",
+        "dark-2": "#232538",
       },
-      
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
-
+    require("@tailwindcss/forms"),
+    require("daisyui")
+],
+};
